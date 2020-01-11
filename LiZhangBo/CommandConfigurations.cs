1d079181356dba4f9f24092fb3deea8fea069599
Moweb <Moweb@MOWEB-WIN10>
2020-01-11 21:28:42 +0800

    implement bitrate limit

--- LiZhangBo/CommandConfigurations.cs
+++ LiZhangBo/CommandConfigurations.cs
@@ -19,13 +19,37 @@
 
         public virtual string SizeLimit
         {
-            get => sizeLimit;
+            get => isFileSizeLimited ? sizeLimit : null;
             set => SetProperty(ref sizeLimit, value);
+        }
+
+        public virtual bool IsFileSizeLimited
+        {
+            get => isFileSizeLimited;
+            set => SetProperty(ref isFileSizeLimited, value, nameof(IsFileSizeLimited), nameof(SizeLimit));
+        }
+
+        public virtual bool IsMaxBitrateLimited
+        {
+            get => isMaxBitrateLimited;
+            set => SetProperty(ref isMaxBitrateLimited, value, nameof(IsMaxBitrateLimited), nameof(BitrateLimit));
+        }
+
+        public virtual string BitrateLimit
+        {
+            get => isMaxBitrateLimited ? bitrateLimit : null;
+            set => SetProperty(ref bitrateLimit, value);
         }
 
         abstract public VideoConfigurations VideoConfiguration { get; set; }
 
         abstract public AudioConfigurations AudioConfiguration { get; set; }
+
+        bool isMaxBitrateLimited = true;
+
+        bool isFileSizeLimited = true;
+
+        string bitrateLimit = "15M";
 
         string sizeLimit = "60M";
 
--- LiZhangBo/MainWindow.xaml.cs
+++ LiZhangBo/MainWindow.xaml.cs
@@ -66,51 +66,70 @@ namespace LiZhangBo
             var cts = (state.Cts = new CancellationTokenSource(timeout));
             var config = Configurations;
             var videoConfig = config.VideoConfiguration;
+            var onExited = new EventHandler((_, e_) =>
+            {
+                try
+                {
+                    state.Task = null;
+                    state.Cts = null;
+                }
+                catch (Exception)
+                {
+                    // never throw
+                }
+            });
             state.Task = Task.Run(() =>
             {
-                var seek = config.Seek.ParseToTimeSpan();
-                var to = config.To.ParseToTimeSpan();
-                if (seek != null && to == null || seek == null && to != null)
-                    throw new ArgumentNullException();
-                var sizeLimit = config.SizeLimit.ParseSize() * 8;
-                var videoSettings = videoConfig.Enabled ? $"-c:v {videoConfig.Codec}{(videoConfig.Is2Passing ? " -pass 1" : string.Empty)}{(seek == null ? string.Empty : $"{sizeLimit / (to - seek).Value.TotalSeconds}".WithSwitch("-b:v"))}" : "-vn";
-                var args = $@"-y {config.Seek.WithSwitch("-ss")}{config.To.WithSwitch("-to")} -i ""{config.SourcePath}"" {videoSettings}{(config.AudioConfiguration.Enabled ? string.Empty : " -an")} ""{config.TargetPath}""";
-                var proc = new Process
+                try
                 {
-                    StartInfo = new ProcessStartInfo
+                    var seek = config.Seek.ParseToTimeSpan();
+                    var to = config.To.ParseToTimeSpan();
+                    if (seek != null && to == null || seek == null && to != null)
+                        throw new ArgumentNullException();
+                    var sizeLimit = seek == null ? null : config.SizeLimit.ParseSize() * 8 / (to - seek).Value.TotalSeconds;
+                    if (sizeLimit.HasValue)
+                        sizeLimit = Math.Round(sizeLimit.Value);
+                    var bitrateLimit = config.BitrateLimit.ParseSize();
+                    if (sizeLimit > bitrateLimit)
+                        sizeLimit = bitrateLimit;
+                    var sizeLimitSwitch = sizeLimit == null ? string.Empty : $"{sizeLimit}".WithSwitch("-b:v");
+                    var videoSettings = videoConfig.Enabled ? $"-c:v {videoConfig.Codec}{(videoConfig.Is2Passing ? " -pass 1" : string.Empty)}{(seek == null ? string.Empty : sizeLimitSwitch)}" : "-vn";
+                    var args = $@"-y {config.Seek.WithSwitch("-ss")}{config.To.WithSwitch("-to")} -i ""{config.SourcePath}"" {videoSettings}{(config.AudioConfiguration.Enabled ? string.Empty : " -an")} ""{config.TargetPath}""";
+                    var proc = new Process
                     {
-                        FileName = config.Executable,
-                        Arguments = args,
-                        RedirectStandardError = true,
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true,
-                    },
-                    EnableRaisingEvents = true
-                };
-                cts.Token.Register(() => proc.Kill());
-                var procOutput = new StringBuilder();
-                proc.OutputDataReceived += (_, e_) =>
-                {
-                    procOutput.AppendLine(e_.Data);
-                };
-                var procError = new StringBuilder();
-                proc.ErrorDataReceived += (_, e_) =>
-                {
-                    procError.AppendLine(e_.Data);
-                    state.ConsoleOutput = procError.ToString();
-                };
-                proc.Exited += (_, e_) =>
-                {
-                    Dispatcher.Invoke(() =>
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = config.Executable,
+                            Arguments = args,
+                            RedirectStandardError = true,
+                            RedirectStandardOutput = true,
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                        },
+                        EnableRaisingEvents = true
+                    };
+                    cts.Token.Register(() => proc.Kill());
+                    var procOutput = new StringBuilder();
+                    proc.OutputDataReceived += (_, e_) =>
                     {
-                        state.Task = null;
-                        state.Cts = null;
-                    });
-                };
-                proc.Start();
-                proc.BeginOutputReadLine();
-                proc.BeginErrorReadLine();
+                        procOutput.AppendLine(e_.Data);
+                    };
+                    var procError = new StringBuilder();
+                    proc.ErrorDataReceived += (_, e_) =>
+                    {
+                        procError.AppendLine(e_.Data);
+                        state.ConsoleOutput = procError.ToString();
+                    };
+                    proc.Exited += onExited;
+                    proc.Start();
+                    proc.BeginOutputReadLine();
+                    proc.BeginErrorReadLine();
+                }
+                catch (Exception ex)
+                {
+                    onExited(this, EventArgs.Empty);
+                    MessageBox.Show(ex.StackTrace, ex.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             });
         }
 
